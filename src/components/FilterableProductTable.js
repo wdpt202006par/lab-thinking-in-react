a9513e8c97c07e56ba1c3cd8a1655dcdd57cef42
Clementine Nocton <clementinenocton@hotmail.fr>
2020-11-05 09:22:21 +0100

    Up

--- src/components/FilterableProductTable.js
+++ src/components/FilterableProductTable.js
@@ -7,11 +7,20 @@ class FilterableProductTable extends React.Component {
         products: this.props.products.data
     }
 
+    searchProduct = (data) => {
+        const filteredProducts = this.props.products.data.filter(product => {
+            product.name.includes(data.search)
+        });
+        this.setState({
+            products: filteredProducts
+        })
+    }
+
     render() {
         return( 
             <div className="container">
                 <h1>IronStore</h1>
-                <SearchBar />
+                <SearchBar searchProduct={this.searchProduct} />
                 <ProductTable products={this.state.products} />
             </div>
         )
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,20 +1,32 @@
 import React from 'react';
 
-const SearchBar = () => {
-    return(
-        <div>
-            <form>
-                <div class="form-group">
-                    <label htmlFor="search">Search</label>
-                    <input type="text" id="search" className="form-control" />
-                </div>
-                <div class="form-group">
-                    <label htmlFor="stock">Only show products on stock</label>
-                    <input type="checkbox" id="stock" className="form-check-label mx-2" />
-                </div>
-                </form>
-        </div>
-    )
+class SearchBar extends React.Component {
+    state = {
+        search: ''
+    };
+
+    isTyping = (e) => {
+        this.setState({
+            search: this.props.searchProduct(this.state)
+        })
+    };
+
+    render() {
+        return(
+            <div>
+                <form>
+                    <div className="form-group">
+                        <label htmlFor="search">Search</label>
+                        <input type="text" id="search" className="form-control" onChange={this.isTyping} />
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="stock">Only show products on stock</label>
+                        <input type="checkbox" id="stock" className="form-check-label mx-2" />
+                    </div>
+                    </form>
+            </div>
+        )
+    }
 }
 
 export default SearchBar;
