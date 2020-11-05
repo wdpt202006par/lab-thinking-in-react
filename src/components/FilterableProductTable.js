14fe726b9cb26fd1eda9fd9aad0e5aa42b8b31b0
CassandreV <cassandre.vanzetta@live.fr>
2020-11-05 19:32:29 +0100

    ite 3 ongoing

--- src/components/FilterableProductTable.js
+++ src/components/FilterableProductTable.js
@@ -2,15 +2,39 @@ import React from 'react';
 import SearchBar from "./SearchBar";
 import ProductTable from "./ProductTable";
 
-const FilterableProductTable = (props) =>{
-    return(
-        <div>
-        <h1>IronSTore</h1>
-        <SearchBar/>
-        <ProductTable productInfilterable ={props.products}/>
-        </div>
-       
-    )
+class FilterableProductTable extends React.Component {
+
+    state = {
+        value:""
+    }
+
+    constructor (props) {
+        super(props);
+        this.filterProducts = this.filterProducts.bind(this);
+    }
+
+    
+    filterProducts(e) {
+        let filteredProducts = this.props.products;
+        this.setState({value:e.target.value});
+        let containsSearch = this.state.value;
+
+        if (containsSearch) {
+            filteredProducts.filter(filteredProduct => filteredProduct.name.includes(containsSearch))
+        }
+    }
+
+    render() { 
+        
+        return(
+            <div>
+                <h1>IronSTore</h1>
+                <SearchBar filterProducts={ () => this.filterProducts() } value={this.state.value} />
+                <ProductTable productInfilterable ={this.props.products}/>
+            </div>
+        )
+    }
+
 }
 
 export default FilterableProductTable;
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -3,14 +3,14 @@ import React from 'react';
 
 class searchBar extends React.Component {
  state={
-     value:"",
+    //  value:"",
      check:false
- }
-
-
-handleChange =(event) =>{
-this.setState({value:event.target.value})
 }
+
+
+// handleChange =(event) =>{
+// this.setState({value:event.target.value})
+// }
 
 handleCheck =(event) =>{
     this.setState({check :!event.target.checked})
@@ -20,9 +20,9 @@ handleCheck =(event) =>{
         return(
             <div>
                 <h2>Search</h2>
-                <input type="text" name="searchbar" value={this.state.value} onChange ={this.handleChange}/>
+                <input type="text" name="searchbar" value={this.props.value} onChange={(e) => {this.props.filterProducts()}}/>
                 <div>
-                <input type="checkbox" name="chackbox" value={this.state.check} onClick ={this.handleCheck}/>
+                <input type="checkbox" name="chackbox" value={this.state.check} onClick={this.handleCheck}/>
                 <p>Only show products on stock</p>
                 </div>              
             </div>
