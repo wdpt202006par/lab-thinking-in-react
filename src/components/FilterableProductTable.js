91790c1ead95885cb5fb4159c01ebc3ba8163d16
Nina Quaresma <quaresma.nina@gmail.com>
2020-11-05 19:23:56 +0100

    Iteration correction

--- src/components/FilterableProductTable.js
+++ src/components/FilterableProductTable.js
@@ -3,45 +3,52 @@ import SearchBar from './SearchBar';
 import ProductTable from './ProductTable';
 
 class FilterableProductTable extends Component {
+  state = {
+    products: this.props.products.data,
+    filteredProductsBySearch: '',
+    inStock: false,
+  };
 
-	state = {
-		products: this.props.products.data,
-		filteredProductsBySearch: '',
-		inStock: false
-	}
-
-	filterProducts = (searchInput) => {
-		const filteredProductArrByInStock = this.state.products.filter(product => product.stocked)
-		let filteredProducts
-
-		this.setState(
-			{filteredProductsBySearch: filteredProducts})
-	}
-	// checkInStock = (searchInput) => {
-	// 	this.setState({ [name]: value },
-	// 			this.handleSubmit)
-	// 			this.handleSubmit()
-	// 	if(searchInput.inStock){
-	// 		filteredProducts = filteredProductArrByInStock.filter(product => product.name.toLowerCase().startsWith(searchInput.search))
-			
-	// 	} else {
-	// 		filteredProducts = this.state.products.filter(product => product.name.toLowerCase().startsWith(searchInput.search))
-	// 	}
-	// }
+  filterProducts = (searchInput) => {
+    const filteredProductArrByInStock = this.state.products.filter(
+      (product) => product.stocked
+    );
+    let filteredProducts;
+    if (searchInput.inStock) {
+      filteredProducts = filteredProductArrByInStock.filter((product) =>
+        product.name.toLowerCase().startsWith(searchInput.search)
+      );
+    } else {
+      filteredProducts = this.state.products.filter((product) =>
+        product.name.toLowerCase().startsWith(searchInput.search)
+      );
+      this.setState({ filteredProductsBySearch: filteredProducts });
+    }
+  };
+  // checkInStock = (searchInput) => {
+  // 	this.setState({ [name]: value },
+  // 			this.handleSubmit)
+  // 			this.handleSubmit()
+  //
+  // 	}
+  // }
 
   render() {
     //console.log('this.props.products', this.props.products);
 
     return (
       <div>
-				<h1>IronStore</h1>
-        <SearchBar products= { this.state.products } 
-				searchTerm = { this.filterProducts} inStock = { this.checkInStock} />
-				{
-					this.state.filteredProductsBySearch !== "" ? <ProductTable products={ this.state.filteredProductsBySearch } /> 
-					: 
-					<ProductTable products= { this.state.products } />
-				}
+        <h1>IronStore</h1>
+        <SearchBar
+          products={this.state.products}
+          searchTerm={this.filterProducts}
+          inStock={this.checkInStock}
+        />
+        {this.state.filteredProductsBySearch !== '' ? (
+          <ProductTable products={this.state.filteredProductsBySearch} />
+        ) : (
+          <ProductTable products={this.state.products} />
+        )}
       </div>
     );
   }
