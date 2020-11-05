a3e41ed89a1b21d3a776d2c064594a2f4763b359
toufiq23 <drrameraaron023@gmail.com>
2020-11-05 19:14:10 +0100

    All done

--- src/components/FilterableProductTable.js
+++ src/components/FilterableProductTable.js
@@ -3,59 +3,45 @@ import SearchBar from './SearchBar';
 import ProductTable from './ProductTable';
 
 class FilterableProductTable extends Component {
-  state = {
-    products: this.props.products.data,
-    filteredProductsBySearch: '',
-    // inStock: false,
-  };
 
-  filterProducts = (searchInput) => {
-    const filteredProductArrByInStock = this.state.products.filter(
-      (product) => product.stocked
-    );
-    let filteredProducts;
+	state = {
+		products: this.props.products.data,
+		filteredProductsBySearch: '',
+		inStock: false
+	}
 
-    if (searchInput.inStock) {
-      filteredProducts = filteredProductArrByInStock.filter((product) =>
-        product.name.toLowerCase().startsWith(searchInput.search)
-      );
-    } else {
-      filteredProducts = this.state.products.filter((product) =>
-        product.name.toLowerCase().startsWith(searchInput.search)
-      );
-    }
-    this.setState({ filteredProductsBySearch: filteredProducts });
-  };
-  //   checkInStock = (searchInput) => {
-  //     searchInput = this.state.inStock de Search Bar
-  //     const filteredProductArrByInStock = [];
-  //     const filteredProducts = [];
-  //     if (searchInput) {
-  //       this.setState({ inStock: true });
-  //     }
-  //     if (this.state.inStock) {
-  //       filteredProducts = this.state.products.filter((product) => (product) =>
-  //         product.stocked === false
-  //       );
-  //     }
-  //   };
+	filterProducts = (searchInput) => {
+		const filteredProductArrByInStock = this.state.products.filter(product => product.stocked)
+		let filteredProducts
+
+		this.setState(
+			{filteredProductsBySearch: filteredProducts})
+	}
+	// checkInStock = (searchInput) => {
+	// 	this.setState({ [name]: value },
+	// 			this.handleSubmit)
+	// 			this.handleSubmit()
+	// 	if(searchInput.inStock){
+	// 		filteredProducts = filteredProductArrByInStock.filter(product => product.name.toLowerCase().startsWith(searchInput.search))
+			
+	// 	} else {
+	// 		filteredProducts = this.state.products.filter(product => product.name.toLowerCase().startsWith(searchInput.search))
+	// 	}
+	// }
 
   render() {
     //console.log('this.props.products', this.props.products);
 
     return (
       <div>
-        <h1>IronStore</h1>
-        <SearchBar
-          products={this.state.products}
-          searchTerm={this.filterProducts}
-          //inStock={this.checkInStock}
-        />
-        {this.state.filteredProductsBySearch !== '' ? (
-          <ProductTable products={this.state.filteredProductsBySearch} />
-        ) : (
-          <ProductTable products={this.state.products} />
-        )}
+				<h1>IronStore</h1>
+        <SearchBar products= { this.state.products } 
+				searchTerm = { this.filterProducts} inStock = { this.checkInStock} />
+				{
+					this.state.filteredProductsBySearch !== "" ? <ProductTable products={ this.state.filteredProductsBySearch } /> 
+					: 
+					<ProductTable products= { this.state.products } />
+				}
       </div>
     );
   }
